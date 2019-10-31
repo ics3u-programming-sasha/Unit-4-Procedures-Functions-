ec86899584de3abb0e939b6ff23ad2048ce209fa
Sasha Malko <sasha.malko@ocsbstudent.ca>
2019-10-31 12:10:49 -0400

    Made changes

---
+++ TempConverterSasha/TempConverterSasha/TempConverterForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TempConverterSasha
+{
+    partial class frmTempConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.AutoSize = true;
+            this.lblEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnter.ForeColor = System.Drawing.Color.White;
+            this.lblEnter.Location = new System.Drawing.Point(67, 89);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(410, 25);
+            this.lblEnter.TabIndex = 0;
+            this.lblEnter.Text = "Enter the temperature in degrees Celsius:";
+            // 
+            // txtCelsius
+            // 
+            this.txtCelsius.Location = new System.Drawing.Point(505, 95);
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsius.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(301, 184);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(157, 59);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert to Fahrenheit";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // frmTempConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtCelsius);
+            this.Controls.Add(this.lblEnter);
+            this.Name = "frmTempConverter";
+            this.Text = "Temperature Converter by Sasha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnter;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ TempConverterSasha/TempConverterSasha/TempConverterForm.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Created by: Sasha Malko
+ * Created on: 30-Oct-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #28 - Temperature Converter
+ * This program gets the degrees Celsius from the user, passes it BY VALUE to a procedure 
+ * that converts it to Fahrenheit, and then inside the procedure displays the degrees Fahrenheit in a message box. 
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TempConverterSasha
+{
+    public partial class frmTempConverter : Form
+    {
+        public frmTempConverter()
+        {
+            InitializeComponent();
+        }
+
+        //Function: Calculatetemp
+        //Input: double celsius
+        //Output: void
+        //Description: This function takes the degrees Celsius passed by value and displays a 
+        //message box with the conversion of Celsius to Fahrenheit.
+        public void CalculateTemp(Double celsius)
+        {
+            // declare local variable
+            Double fahrenheit;
+
+            // convert the string in the text box to a double
+            celsius = double.Parse(txtCelsius.Text);
+
+            // calculate the conversion of degrees Celsius into Fahrenheit
+            fahrenheit = (double)9 / (double)5 * celsius + 32;
+
+            // display a message box with the conversion of Celsius to Fahrenheit
+            MessageBox.Show(celsius + " degrees Celsius = " + fahrenheit + " degrees Fahrenheit", "Celsius to Fahrenheit Converter");
+        }
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            Double userCelsius;
+
+            // convert the string in the text box to a double
+            userCelsius = Convert.ToDouble(txtCelsius.Text);
+
+            // call the funtion to calculate the conversion passing the userCelsius argument by value
+            this.CalculateTemp(userCelsius);
+        }
+    }
+}

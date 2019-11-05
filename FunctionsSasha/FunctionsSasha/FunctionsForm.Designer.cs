79f34fe160b5764ca3efa098b2fcebf0822f5021
Sasha Malko <sasha.malko@ocsbstudent.ca>
2019-11-05 13:07:27 -0500

    Worked on functions

---
+++ FunctionsSasha/FunctionsSasha/FunctionsForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace FunctionsSasha
+{
+    partial class frmFunctions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRadius.Location = new System.Drawing.Point(249, 74);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(90, 23);
+            this.lblRadius.TabIndex = 0;
+            this.lblRadius.Text = "Radius: ";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(410, 79);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 1;
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.Location = new System.Drawing.Point(249, 157);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(81, 23);
+            this.lblHeight.TabIndex = 2;
+            this.lblHeight.Text = "Height:";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(410, 160);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(314, 237);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(120, 54);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate Volume";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVolume.Location = new System.Drawing.Point(232, 365);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(0, 23);
+            this.lblVolume.TabIndex = 5;
+            // 
+            // frmFunctions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.lblRadius);
+            this.Name = "frmFunctions";
+            this.Text = "Functions by Sasha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblVolume;
+    }
+}
+
---
+++ FunctionsSasha/FunctionsSasha/FunctionsForm.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Created by: Sasha Malko
+ * Created on: 5-Nov-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #30 - Volume of a Cylinder
+ * This program accepts 2 inputs and calculates the volume of a cylinder. 
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunctionsSasha
+{
+    public partial class frmFunctions : Form
+    {
+        public frmFunctions()
+        {
+            InitializeComponent();
+        }
+
+        // Function: CalculateVolume
+        // Input: double radius, double height
+        // Output: double
+        // Description: This function calculates the volume of a cylinder with the given radius and height. 
+        private double CalculateVolume(double radius, double height)
+        {
+            // declare local variable
+            double volume; 
+
+            // calculate the volume of the cylinder
+            volume = Math.PI * Math.Pow(radius, 2) * height;
+
+            // return the volume
+            return volume;
+        }
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            double radius, height, userVolume;
+
+            // get the radius and height from the user
+            radius = double.Parse(txtRadius.Text);
+            height = double.Parse(txtHeight.Text);
+
+            // call the function  to calculate the volume of the cylinder
+            userVolume = CalculateVolume(radius, height);
+
+            // display the volume of the cylinder
+            this.lblVolume.Text = "The volume is: " + String.Format("{0:0.00}", userVolume) + " cubic units"; 
+        }
+    }
+}

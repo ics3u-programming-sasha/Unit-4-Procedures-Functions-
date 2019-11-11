ab7e9b3f54649e962ae3e10c2b16f7c169d3a3cc
Sasha Malko <sasha.malko@ocsbstudent.ca>
2019-11-11 13:02:56 -0500

    Worked on switch statement

---
+++ SwitchStatementSasha/SwitchStatementSasha/SwitchStatementForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SwitchStatementSasha
+{
+    partial class frmSwitchStatement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.txtLevel = new System.Windows.Forms.MaskedTextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.AutoSize = true;
+            this.lblEnter.Font = new System.Drawing.Font("DejaVu Sans Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnter.Location = new System.Drawing.Point(219, 79);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(136, 24);
+            this.lblEnter.TabIndex = 0;
+            this.lblEnter.Text = "Enter a level:";
+            // 
+            // txtLevel
+            // 
+            this.txtLevel.Location = new System.Drawing.Point(396, 83);
+            this.txtLevel.Name = "txtLevel";
+            this.txtLevel.Size = new System.Drawing.Size(100, 20);
+            this.txtLevel.TabIndex = 2;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(320, 190);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(91, 35);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // frmSwitchStatement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtLevel);
+            this.Controls.Add(this.lblEnter);
+            this.Name = "frmSwitchStatement";
+            this.Text = "Switch Statement by Sasha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnter;
+        private System.Windows.Forms.MaskedTextBox txtLevel;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ SwitchStatementSasha/SwitchStatementSasha/SwitchStatementForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwitchStatementSasha
+{
+    public partial class frmSwitchStatement : Form
+    {
+        public frmSwitchStatement()
+        {
+            InitializeComponent();
+        }
+
+        private int ConvertToPercent(String level)
+        {
+            int percentage;
+
+            switch (level)
+            {
+                case "4++":
+                    percentage = 98;
+                    break;
+                case "4+":
+                    percentage = 95;
+                    break;
+                case "4":
+                    percentage = 87;
+                    break;
+                case "4-":
+                    percentage = 83;
+                    break;
+                case "3+":
+                    percentage = 78;
+                    break;
+                case "3":
+                    percentage = 75;
+                    break;
+                case "3-":
+                    percentage = 75;
+                    break;
+
+            }
+        }
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

58eeec71d9ee0851c38db02f1b459ec8bff6b4c1
Sasha Malko <sasha.malko@ocsbstudent.ca>
2019-11-20 13:01:55 -0500

    Worked on string equality

---
+++ StringEqualitySasha/StringEqualitySasha/StringEqualityForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace StringEqualitySasha
+{
+    partial class frmStringEquality
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.lblCase = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblString2 = new System.Windows.Forms.Label();
+            this.txtString1 = new System.Windows.Forms.TextBox();
+            this.txtString2 = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblEqual = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.AutoSize = true;
+            this.lblEnter.Font = new System.Drawing.Font("Complex_IV50", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnter.Location = new System.Drawing.Point(77, 32);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(592, 27);
+            this.lblEnter.TabIndex = 0;
+            this.lblEnter.Text = "Enter two strings to see if they are equal.";
+            // 
+            // lblCase
+            // 
+            this.lblCase.AutoSize = true;
+            this.lblCase.Font = new System.Drawing.Font("Complex_IV50", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCase.Location = new System.Drawing.Point(78, 72);
+            this.lblCase.Name = "lblCase";
+            this.lblCase.Size = new System.Drawing.Size(258, 23);
+            this.lblCase.TabIndex = 1;
+            this.lblCase.Text = "(Case does not matter.)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Complex_IV50", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(212, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "String1:";
+            // 
+            // lblString2
+            // 
+            this.lblString2.AutoSize = true;
+            this.lblString2.Font = new System.Drawing.Font("Complex_IV50", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblString2.Location = new System.Drawing.Point(212, 213);
+            this.lblString2.Name = "lblString2";
+            this.lblString2.Size = new System.Drawing.Size(98, 23);
+            this.lblString2.TabIndex = 3;
+            this.lblString2.Text = "String2:";
+            // 
+            // txtString1
+            // 
+            this.txtString1.Location = new System.Drawing.Point(413, 160);
+            this.txtString1.Name = "txtString1";
+            this.txtString1.Size = new System.Drawing.Size(100, 20);
+            this.txtString1.TabIndex = 4;
+            // 
+            // txtString2
+            // 
+            this.txtString2.Location = new System.Drawing.Point(413, 213);
+            this.txtString2.Name = "txtString2";
+            this.txtString2.Size = new System.Drawing.Size(100, 20);
+            this.txtString2.TabIndex = 5;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(319, 285);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(91, 35);
+            this.btnCheck.TabIndex = 6;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // lblEqual
+            // 
+            this.lblEqual.AutoSize = true;
+            this.lblEqual.Font = new System.Drawing.Font("Complex_IV50", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEqual.Location = new System.Drawing.Point(212, 387);
+            this.lblEqual.Name = "lblEqual";
+            this.lblEqual.Size = new System.Drawing.Size(22, 23);
+            this.lblEqual.TabIndex = 7;
+            this.lblEqual.Text = "?";
+            // 
+            // frmStringEquality
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEqual);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtString2);
+            this.Controls.Add(this.txtString1);
+            this.Controls.Add(this.lblString2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCase);
+            this.Controls.Add(this.lblEnter);
+            this.Name = "frmStringEquality";
+            this.Text = "String Equality by Sasha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnter;
+        private System.Windows.Forms.Label lblCase;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblString2;
+        private System.Windows.Forms.TextBox txtString1;
+        private System.Windows.Forms.TextBox txtString2;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblEqual;
+    }
+}
+

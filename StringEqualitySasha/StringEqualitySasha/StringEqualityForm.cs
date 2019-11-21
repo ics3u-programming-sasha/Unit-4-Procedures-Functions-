eb95d29e208cbf7140d20948d171e3c79f75634e
Sasha Malko <36854156+SashaMalko0810@users.noreply.github.com>
2019-11-21 15:54:29 -0800

    Made changes

--- StringEqualitySasha/StringEqualitySasha/StringEqualityForm.cs
+++ StringEqualitySasha/StringEqualitySasha/StringEqualityForm.cs
@@ -19,7 +19,21 @@ namespace StringEqualitySasha
 
         private void BtnCheck_Click(object sender, EventArgs e)
         {
+            string string1, string2, stringUppercase1, stringUppercase2;
+            int stringLength1, stringLength2;
+            bool isEqual = false;
 
+            string1 = this.txtString1.Text;
+            string2 = this.txtString1.Text; 
+
+            stringLength1 = string1.Length;
+            stringLength2 = string2.Length; 
+
+            if (stringLength1 == stringLength2)
+            {
+                stringUppercase1 = string1.ToUpper();
+                stringUppercase2 = string2.ToUpper();
+            }
         }
     }
 }

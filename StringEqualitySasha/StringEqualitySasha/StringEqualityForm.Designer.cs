30728096ef7988fca4452c3e61311628e4b2dab5
Sasha Malko <36854156+SashaMalko0810@users.noreply.github.com>
2019-11-21 17:03:15 -0800

    Made changes

--- StringEqualitySasha/StringEqualitySasha/StringEqualityForm.Designer.cs
+++ StringEqualitySasha/StringEqualitySasha/StringEqualityForm.Designer.cs
@@ -41,40 +41,40 @@
             // lblEnter
             // 
             this.lblEnter.AutoSize = true;
-            this.lblEnter.Font = new System.Drawing.Font("Complex_IV50", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEnter.Location = new System.Drawing.Point(77, 32);
             this.lblEnter.Name = "lblEnter";
-            this.lblEnter.Size = new System.Drawing.Size(592, 27);
+            this.lblEnter.Size = new System.Drawing.Size(389, 24);
             this.lblEnter.TabIndex = 0;
             this.lblEnter.Text = "Enter two strings to see if they are equal.";
             // 
             // lblCase
             // 
             this.lblCase.AutoSize = true;
-            this.lblCase.Font = new System.Drawing.Font("Complex_IV50", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCase.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCase.Location = new System.Drawing.Point(78, 72);
             this.lblCase.Name = "lblCase";
-            this.lblCase.Size = new System.Drawing.Size(258, 23);
+            this.lblCase.Size = new System.Drawing.Size(176, 20);
             this.lblCase.TabIndex = 1;
             this.lblCase.Text = "(Case does not matter.)";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Complex_IV50", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(212, 157);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 23);
+            this.label2.Size = new System.Drawing.Size(72, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "String1:";
             // 
             // lblString2
             // 
             this.lblString2.AutoSize = true;
-            this.lblString2.Font = new System.Drawing.Font("Complex_IV50", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblString2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblString2.Location = new System.Drawing.Point(212, 213);
             this.lblString2.Name = "lblString2";
-            this.lblString2.Size = new System.Drawing.Size(98, 23);
+            this.lblString2.Size = new System.Drawing.Size(72, 20);
             this.lblString2.TabIndex = 3;
             this.lblString2.Text = "String2:";
             // 
@@ -106,12 +106,11 @@
             // lblEqual
             // 
             this.lblEqual.AutoSize = true;
-            this.lblEqual.Font = new System.Drawing.Font("Complex_IV50", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEqual.Location = new System.Drawing.Point(212, 387);
+            this.lblEqual.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEqual.Location = new System.Drawing.Point(265, 386);
             this.lblEqual.Name = "lblEqual";
-            this.lblEqual.Size = new System.Drawing.Size(22, 23);
+            this.lblEqual.Size = new System.Drawing.Size(0, 20);
             this.lblEqual.TabIndex = 7;
-            this.lblEqual.Text = "?";
             // 
             // frmStringEquality
             // 
--- StringEqualitySasha/StringEqualitySasha/StringEqualityForm.cs
+++ StringEqualitySasha/StringEqualitySasha/StringEqualityForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Sasha Malko
+ * Created on: 21-Nov-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #35 - String Equality
+ * This program checks if 2 strings are the same (if you ignore capitals and smalls), 
+ * and returns a boolean "bool" value. 
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,23 +25,68 @@ namespace StringEqualitySasha
             InitializeComponent();
         }
 
-        private void BtnCheck_Click(object sender, EventArgs e)
+        // Function: StringsAreEqual
+        // Input: string string1, string string2
+        // Output: bool
+        // Description: This function checks if the two given strings are equal without considering cases. 
+        private bool StringsAreEqual(string string1, string string2)
         {
-            string string1, string2, stringUppercase1, stringUppercase2;
+            // set the boolean variable equal to false
+            bool isEqual = false; 
+
+            // declare local variables;
             int stringLength1, stringLength2;
-            bool isEqual = false;
 
-            string1 = this.txtString1.Text;
-            string2 = this.txtString1.Text; 
-
+            // get the lengths of the strings
             stringLength1 = string1.Length;
-            stringLength2 = string2.Length; 
+            stringLength2 = string2.Length;
 
+            // check if the strings are equal in length
             if (stringLength1 == stringLength2)
             {
-                stringUppercase1 = string1.ToUpper();
-                stringUppercase2 = string2.ToUpper();
+                // declare local variables
+                string stringCase1, stringCase2; 
+
+                // convert the strings to uppercase
+                stringCase1 = string1.ToUpper();
+                stringCase2 = string2.ToUpper();
+
+                // check if the strings are equal without considering cases
+                if (stringCase1 == stringCase2)
+                {
+                    // set the boolean variable equal to true
+                    isEqual = true;
+                }
             }
+
+            // return the boolean variable
+            return isEqual;
+        }
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            string uString1, uString2;
+            bool equal;
+
+            // get the strings from the text boxes
+            uString1 = this.txtString1.Text;
+            uString2 = this.txtString2.Text;
+
+            // call the function to check if the strings are equal
+            equal = StringsAreEqual(uString1, uString2);
+
+            // check if the boolean variable equals true
+            if (equal == true)
+            {
+                // display that the strings are equal
+                this.lblEqual.Text = "These strings are equal.";
+            }
+            else
+            {
+                // display that the strings are not equal
+                this.lblEqual.Text = "These strings are NOT equal.";
+            }
+
         }
     }
 }
